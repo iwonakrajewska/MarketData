39a35dd5daae4434debd82d5da7c4402f66005d8
me <krajewska@gmail.com>
2018-03-05 00:21:36 +0000

    adding linux executors

---
+++ LoanAlert/go.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./myprogram2.sh http://alert-generation-question.rockall-laser.com/ffc9c8e9-f929-46db-ac5c-7c483c647568/marketdata.json http://alert-generation-question.rockall-laser.com/ffc9c8e9-f929-46db-ac5c-7c483c647568/loandata.json http://alert-generation-question.rockall-laser.com/ffc9c8e9-f929-46db-ac5c-7c483c647568/creditpolicy.json
---
+++ MarketData/go.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./myprogram.sh http://alert-generation-question.rockall-laser.com/ffc9c8e9-f929-46db-ac5c-7c483c647568/marketdata.json
